f57a2b99490aa2173d691e36e8f409447097f4c4
KaiGai Kohei <kaigai@kaigai.gr.jp>
2011-07-16 03:45:54 +0200

    a workaround

--- pg_boost.h
+++ pg_boost.h
@@ -1,7 +1,7 @@
 /*
  * pg_boost.h - Header file of pg_boost module
  *
- * Copyright (c) KaiGai Kohei <kaigai@kaigai.gr.jp>
+ * Copyright (c) 2011 KaiGai Kohei <kaigai@kaigai.gr.jp>
  *
  * 
  */
@@ -9,20 +9,18 @@
 #define PG_BOOST_H
 #include "postgres.h"
 #include <pthread.h>
-#include <stdint.h>
-#include <stdio.h>
 
 /*
- * offset_t - offset from the head of shared memory segment
+ * Pointer on the shared memory segment
  */
-typedef uint64	offset_t;
+typedef uintptr_t	shmptr_t;
 
 /*
  * Dual linked list on shared memory segment
  */
 typedef struct {
-	offset_t	prev;
-	offset_t	next;
+	offset_t		prev;
+	offset_t		next;
 } shmlist_t;
 
 #define offset_of(type, member)					\
@@ -53,51 +51,24 @@ typedef struct {
 		 entry = temp,													\
 		 temp = list_entry(entry->member.next, typeof(*entry), member))
 
-extern bool			shmlist_empty(shmlist_t *list);
-extern void			shmlist_init(shmlist_t *list);
-extern void			shmlist_add(shmlist_t *base, shmlist_t *list);
-extern void			shmlist_del(shmlist_t *list);
+extern bool		shmlist_empty(shmlist_t *list);
+extern void		shmlist_init(shmlist_t *list);
+extern void		shmlist_add(shmlist_t *base, shmlist_t *list);
+extern void		shmlist_del(shmlist_t *list);
 
 /*
  * Management of shared memory segment
  */
-extern bool			shmmgr_init(size_t size, bool hugetlb);
-extern void			shmmgr_exit(void);
-extern void		   *shmmgr_alloc(size_t size);
-extern void			shmmgr_free(void *ptr);
-extern bool			shmmgr_init_mutex(pthread_mutex_t *lock);
-extern bool			shmmgr_init_rwlock(pthread_rwlock_t *lock);
-extern offset_t		addr_to_offset(void *addr);
-extern void		   *offset_to_addr(offset_t offset);
-
-extern void		   *shmmgr_get_bufmgr_head(void);
-extern size_t		shmmgr_get_size(void *ptr);
-
-/*
- * Shared buffer management
- */
-#define SHMBUF_FLAGS_DIRTY_CACHE	0x01
-#define SHMBUF_FLAGS_HOT_CACHE		0x02
-#define SHMBUF_FLAGS_COMPRESSED		0x04
-
-typedef struct {
-	offset_t	storage;
-	offset_t	cached;
-	shmlist_t	list;
-	size_t		size;
-	int			refcnt;
-	uint16		index;
-	uint8		tag;
-	uint8		flags;
-	pthread_mutex_t lock;
-} shmbuf_t;
-
-extern bool		shmbuf_create(shmbuf_t *shmbuf, int tag, size_t size);
-extern void		shmbuf_delete(shmbuf_t *shmbuf);
-extern void		shmbuf_reclaim(size_t size);
-extern void	   *shmbuf_get_buffer(shmbuf_t *shmbuf);
-extern void		shmbuf_put_buffer(shmbuf_t *shmbuf);
-extern void		shmbuf_set_dirty(shmbuf_t *shmbuf);
-extern offset_t	shmbuf_init(size_t size);
+extern shmptr_t	shmmgr_alloc(size_t size, bool is_buffer);
+extern shmptr_t	shmmgr_try_alloc(size_t size, bool is_buffer);
+extern void		shmmgr_free(shmptr_t ptr);
+extern void		shmmgr_init_mutex(pthread_mutex_t *lock);
+extern void		shmmgr_init_rwlock(pthread_rwlock_t *lock);
+extern void	   *shmmgr_get_addr(shmptr_t ptr);
+extern void		shmmgr_put_addr(shmptr_t ptr);
+extern void		shmmgr_get_size(shmptr_t ptr);
+extern void		shmmgr_set_dirty(shmptr_t ptr);
+extern void		shmmgr_init(int nsegments, size_t segment_size);
+extern void		shmmgr_exit(void);
 
 #endif	/* PG_BOOST_H */
--- shmmgr.c
+++ shmmgr.c
@@ -1,4 +1,235 @@
 /*
+ * shmmgr.c
+ *
+ * Routines to manage shared memory segment
+ *
+ * Copyright (c) 2011, KaiGai Kohei
+ */
+#include "pg_boost.h"
+
+
+
+/*
+ * shmchunk_t - memory chunk of shared memory segment
+ */
+#define SHMBUFFER_FLAG_DIRTY_CACHE	0x01
+#define SHMBUFFER_HOT_CACHE			0x02
+#define SHMBUFFER_COMPRESSED		0x04
+#define SHMBUFFER_NUM_ACTIVE_SLOTS	64
+typedef struct {
+	uint16				mclass;
+	bool				is_free;
+	bool				is_buffer;
+	union {
+		struct {
+			mlist_t		list;
+		} free;		/* is_free == true */
+		struct {
+			uint8		data[0];
+		} item;
+		struct {	/* is_free == false && is_buffer == false */
+			uintptr_t	storage;
+			uintptr_t	cached;
+			uint32		length;
+			uint32		refcnt;
+			mlist_t		list;
+			uint16		index;
+			uint8		tag;
+			uint8		flags;
+		} buffer;	/* is_free == false && is_buffer == true */
+	};
+} shmchunk_t;
+
+/*
+ * Shared memory segment header
+ */
+#define SHMCLASS_MIN_BITS		5	/* 32bytes */
+#define SHMCLASS_MAX_BITS		30	/* 1Gbytes */
+#define SHMCLASS_MIN_SIZE		(1 << SHMCLASS_MIN_BITS)
+#define SHMCLASS_MAX_SIZE		(1 << SHMCLASS_MAX_BITS)
+
+typedef struct {
+	int			shmid;
+	int			index;
+	size_t		segment_size;
+	mlist_t		free_list[SHMCLASS_MAX_BITS + 1];
+	int			num_active[SHMCLASS_MAX_BITS + 1];
+	int			num_free[SHMCLASS_MAX_BITS + 1];
+	pthread_mutex_t	lock;
+
+	/* buffering support */
+	struct {
+		mlist_t		active_list[SHMBUFFER_NUM_ACTIVE_SLOTS];
+		pthread_mutex_t	active_lock[SHMBUFFER_NUM_ACTIVE_SLOTS];
+		int			buffer_hint;
+		int			reclaim_hint;
+		size_t		total_size;
+		size_t		limit_size;
+	} buffer;
+} shmsegment_t;
+
+#ifdef	SIZEOF_VOID_P == 8
+#define SHMSEGMENT_NUM_MAX_BITS		12		/* 4096 */
+#define SHMSEGMENT_SIZE_MAX_BITS	36		/* 64GB */
+#elseif SIZEOF_VOID_P == 4
+#define SHMSEGMENT_NUM_MAX_BITS		0		/* No multisegment support */
+#define SHMSEGMENT_SIZE_MAX_BITS	32		/* 4 GB */
+#endif
+#define SHMSEGMENT_NUM_MAX			(1 << SHMSEGMENT_NUM_MAX_BITS)
+#define SHMSEGMENT_SIZE_MAX			(1 << SHMSEGMENT_SIZE_MAX)
+#define shmptr_to_segment(p)		(((p) >> SHMSEGMENT_SIZE_MAX_BITS) & (SHMSEGMENT_SIZE_MAX - 1)
+#define shmptr_to_offset(p)			((p) & (SHMSEGMENT_SIZE_MAX - 1))
+
+static msegment_t **msegments;
+static int			msegments_num;
+
+
+
+
+extern uintptr_t	shmmgr_alloc(size_t size, bool is_buffer);
+extern uintptr_t	shmmgr_try_alloc(size_t size, bool is_buffer);
+extern void			shmmgr_free(uintptr_t ptr);
+extern void			shmmgr_init_mutex(pthread_mutex_t *lock);
+extern void			shmmgr_init_rwlock(pthread_rwlock_t *lock);
+extern void		   *shmmgr_get_addr(uintptr_t ptr);
+extern void			shmmgr_put_addr(uintptr_t ptr);
+extern void			shmmgr_get_size(uintptr_t ptr);
+extern void			shmmgr_set_dirty(uintptr_t ptr);
+extern void			shmmgr_init(int nsegments, size_t segment_size);
+extern void			shmmgr_exit(void);
+
+/*
+ * shmmgr_init
+ *
+ *
+ */
+static void
+shmmgr_init_one(msegment_t *msegment)
+{
+	int			mclass;
+	shmptr_t	offset;
+
+	for (mclass = 0; mclass <= SHMCLASS_MAX_BITS; mclass++)
+	{
+		shmlist_init(&msegment->free_list[mclass]);
+		msegment->num_free[mclass] = 0;
+		msegment->num_active[mclass] = 0;
+	}
+
+	offset = 1 << (fls64(sizeof(shmhead_t)) + 1);
+	if (offset < SHMCLASS_MIN_SIZE)
+		offset = SHMCLASS_MIN_SIZE;
+
+	while (msegment->segment_size - offset >= SHMCLASS_MIN_SIZE)
+	{
+		shmchunk_t *chunk;
+
+		/* choose an appropriate chunk class */
+		mclass = ffs(offset) - 1;
+		if (mclass > SHMCLASS_MAX_BITS)
+			mclass = SHMCLASS_MAX_BITS;
+		Assert(mclass >= SHMCLASS_MIN_BITS);
+
+		/* if (offset + chunk_size) over the tail, truncate it */
+		while (msegment->segment_size < offset + (1 << mclass))
+			mclass--;
+
+		if (mclass < SHMCLASS_MIN_BITS)
+			break;
+
+		/* chain this chunk to free_list */
+		chunk = (shmchunk_t *)((uintptr_t)(msegment) + offset);
+		chunk->mclass = mclass;
+		chunk->is_free = true;
+
+		shmlist_add(&msegment->free_list[mclass], &chunk->list);
+		msegment->num_free[mclass]++;
+
+		offset += (1 << mclass);
+	}
+
+
+
+
+
+
+
+
+	}
+	
+
+
+
+
+
+
+
+
+
+
+}
+
+void
+shmmgr_init(int nsegments, size_t segment_size)
+{
+	int		shmid;
+	int		shmflags = 0600 | IPC_CREAT | IPC_EXCL;
+	int		index;
+
+	msegments = malloc(sizeof(msegment_t *) * nsegments);
+	if (!msegments)
+		elog(ERROR, "out of memory");
+
+	for (index = 0; index < nsegments; index++)
+	{
+		shmid = shmget(IPC_PRIVATE, size, shmflag);
+		if (shmid < 0)
+			goto error;
+
+		msegments[index] = shmat(shmid, NULL, 0);
+
+		/*
+		 * To make sure the shared memory segment being released
+		 * when this process exit. If shmat(2) was failed, this
+		 * segment shall be removed soon, because nobody maps it.
+		 */
+		shmctl(shmid, IPC_RMID, NULL);
+
+		if (shmhead == (void *)(-1))
+			goto error;
+
+		msegments[index].shmid = shmid;
+		msegments[index].index = index;
+		msegments[index].segment_size = segment_size;
+		shmmgr_init_one(msegments[index]);
+	}
+	return;
+
+error:
+	while (--index >= 0)
+		shmdt(msegments[index]);
+
+	elog(ERROR,
+		 (errcode(ERRCODE_OUT_OF_MEMORY),
+		  errmsg("could not map shared memory segment")));
+}
+
+/*
+ * shmmgr_exit
+ *
+ */
+void
+shmmgr_exit(void)
+{
+
+
+}
+
+
+
+
+#if 0
+/*
  * shmmgr.c - shared memory management
  *
  * Copyright (c) KaiGai Kohei <kaigai@kaigai.gr.jp>
@@ -482,7 +713,7 @@ shmmgr_init(size_t size, bool hugetlb)
 
 	return true;
 }
-
+#endif
 #if 0
 /*
  * Routines to module testing
