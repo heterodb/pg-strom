6b65c9c6353ac2568475c20a0b60cb86f539a3bf
KaiGai Kohei <kaigai@kaigai.gr.jp>
2014-11-11 11:04:43 +0900

    update license stuff and source code header.

--- mqueue.c
+++ mqueue.c
@@ -6,9 +6,14 @@
  * Copyright 2011-2014 (C) KaiGai Kohei <kaigai@kaigai.gr.jp>
  * Copyright 2014 (C) The PG-Strom Development Team
  *
- * This software is an extension of PostgreSQL; You can use, copy,
- * modify or distribute it under the terms of 'LICENSE' included
- * within this package.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 #include "postgres.h"
 #include "catalog/pg_type.h"
