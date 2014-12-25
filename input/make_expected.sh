cf112c44dac030e147a6eb5140091e9c42fd6968
onlyindreams <bench.coffee@gmail.com>
2014-12-25 19:34:52 +0900

    add comment to make_expected.sh

--- input/make_expected.sh
+++ input/make_expected.sh
@@ -1,25 +1,27 @@
 #! /bin/sh
 
-#####################################################################
-# PG-strom Regression Test cases                                    #
-#####################################################################
+#######################
+# Test cases Maker    #
+#######################
+
+# Add test case names you want to test.
 targets=(agg_init explain_agg group_agg nogrp_agg overflow_agg where_agg zero_agg)
 
 echo "target files to make are...."
 echo "***        ${targets[*]}         ****"
 
-echo "go"
+# Make DUMMY expected files. ( because pg_regress needs expected files )
 for target in "${targets[@]}"
 do
 	touch expected/${target}.out
 done
 
-##############################################################################
-#  Execute PG-strom Regression Test without PG-strom  on temp-check install  #
-##############################################################################
+######################################################################################
+#  Execute PG-strom Regression Test *** without PG-strom *** on temp-check install   #
+######################################################################################
 ../../src/test/regress/pg_regress --top-builddir=../.. --extra-install=contrib/pg_strom --inputdir=input --temp-install=tmp_check --temp-config=input/disable.conf ${targets[*]}
 
-
+# Copy result files to expected directory.
 for target in "${targets[@]}"
 do
 	cp -f results/${target}.out expected
