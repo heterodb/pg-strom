75bf2c4660602a661928fc4698288b51a3f4abb4
onlyindreams <bench.coffee@gmail.com>
2015-01-30 18:44:50 +0900

    a workaround commit

--- test/sql/bugfix_ghj.sql
+++ test/sql/bugfix_ghj.sql
@@ -13,10 +13,10 @@ set enable_bitmapscan to off;
 set client_min_messages to warning;
 
 -- #109
-drop table ttest;
-create table ttest(id integer,c1 real,c2 text);
-alter table ttest alter c2 set storage external;
-insert into ttest select generate_series(1,1),1000,repeat('a',3000)||generate_series(1,10);
+drop table if exists i109;
+create table i109(id integer,c1 real,c2 text);
+alter table i109 alter c2 set storage external;
+insert into i109 select generate_series(1,100),1000,repeat('a',3000)||generate_series(1,10);
 
-select a.id,b.id,a.c1,b.c1,substring(a.c2,1,1),substring(b.c2,1,10) from ttest a inner join ttest b on a.id=b.id;
+select a.id,b.id,a.c1,b.c1,substring(a.c2,1,10),substring(b.c2,1,10) from i109 a inner join i109 b on a.id=b.id;
 
