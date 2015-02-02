e3bad779c8dd7a065f311937df5bce780e9c86c1
onlyindreams <bench.coffee@gmail.com>
2015-02-02 15:29:18 +0900

    Add GpuScan Test case with CPU Recheck.

---
+++ test/sql/recheck_gs.sql
@@ -0,0 +1,67 @@
+--#
+--#       Gpu Scan TestCases with CPU Recheck
+--#
+
+set client_min_messages to notice;
+
+-- NO RECHECK
+select * from strom_test where smlint_x<1e+48 and id%1000=0 order by id;
+select * from strom_test where smlint_x>-1e+48 and id%1000=0 order by id;
+select * from strom_test where smlint_x>1e-32 and id%1000=0 order by id;
+select * from strom_test where smlint_x<-1e-32 and id%1000=0 order by id;
+
+select * from strom_test where integer_x<1e+48 and id%1000=0 order by id;
+select * from strom_test where integer_x>-1e+48 and id%1000=0 order by id;
+select * from strom_test where integer_x>1e-32 and id%1000=0 order by id;
+select * from strom_test where integer_x<-1e-32 and id%1000=0 order by id;
+
+select * from strom_test where bigint_x<1e+48 and id%1000=0 order by id;
+select * from strom_test where bigint_x>-1e+48 and id%1000=0 order by id;
+select * from strom_test where bigint_x>1e-32 and id%1000=0 order by id;
+select * from strom_test where bigint_x<-1e-32 and id%1000=0 order by id;
+
+select * from strom_test where real_x::numeric<1e+10 and id%1000=0 order by id;
+select * from strom_test where real_x::numeric>-1e+10 and id%1000=0 order by id;
+select * from strom_test where real_x::numeric>1e-10 and id%1000=0 order by id;
+select * from strom_test where real_x::numeric<-1e-10 and id%1000=0 order by id;
+
+select * from strom_test where float_x::numeric<1e+10 and id%1000=0 order by id;
+select * from strom_test where float_x::numeric>-1e+10 and id%1000=0 order by id;
+select * from strom_test where float_x::numeric>1e-10 and id%1000=0 order by id;
+select * from strom_test where float_x::numeric<-1e-10 and id%1000=0 order by id;
+
+select * from strom_test where nume_x<1e+10 and id%1000=0 order by id;
+select * from strom_test where nume_x>-1e+10 and id%1000=0 order by id;
+select * from strom_test where nume_x>1e-10 and id%1000=0 order by id;
+select * from strom_test where nume_x<-1e-10 and id%1000=0 order by id;
+
+-- RECHECKED BY CPU.
+select * from strom_test where smlint_x<1e+49 and id%1000=0 order by id;
+select * from strom_test where smlint_x>-1e+49 and id%1000=0 order by id;
+select * from strom_test where smlint_x>1e-33 and id%1000=0 order by id;
+select * from strom_test where smlint_x<-1e-33 and id%1000=0 order by id;
+
+select * from strom_test where integer_x<1e+49 and id%1000=0 order by id;
+select * from strom_test where integer_x>-1e+49 and id%1000=0 order by id;
+select * from strom_test where integer_x>1e-33 and id%1000=0 order by id;
+select * from strom_test where integer_x<-1e-33 and id%1000=0 order by id;
+
+select * from strom_test where bigint_x<1e+49 and id%1000=0 order by id;
+select * from strom_test where bigint_x>-1e+49 and id%1000=0 order by id;
+select * from strom_test where bigint_x>1e-33 and id%1000=0 order by id;
+select * from strom_test where bigint_x<-1e-33 and id%1000=0 order by id;
+
+select * from strom_test where real_x::numeric<1e+1000 and id%1000=0 order by id;
+select * from strom_test where real_x::numeric>-1e+1000 and id%1000=0 order by id;
+select * from strom_test where real_x::numeric>1e-1000 and id%1000=0 order by id;
+select * from strom_test where real_x::numeric<-1e-1000 and id%1000=0 order by id;
+
+select * from strom_test where float_x::numeric<1e+1000 and id%1000=0 order by id;
+select * from strom_test where float_x::numeric>-1e+1000 and id%1000=0 order by id;
+select * from strom_test where float_x::numeric>1e-1000 and id%1000=0 order by id;
+select * from strom_test where float_x::numeric<-1e-1000 and id%1000=0 order by id;
+
+select * from strom_test where nume_x<1e+1000 and id%1000=0 order by id;
+select * from strom_test where nume_x>-1e+1000 and id%1000=0 order by id;
+select * from strom_test where nume_x>1e-1000 and id%1000=0 order by id;
+select * from strom_test where nume_x<-1e-1000 and id%1000=0 order by id;
