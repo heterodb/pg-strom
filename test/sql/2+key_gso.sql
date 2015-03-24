ccf4343293955010d614a22549313968e990cf4f
onlyindreams <bench.coffee@gmail.com>
2015-03-24 11:12:28 +0900

    Add GpuSort regression test-cases.

---
+++ test/sql/2+key_gso.sql
@@ -0,0 +1,15 @@
+---#
+---#    GpuSort Closed Issue (Remapping of variable) Test Cases
+---#    #137, ...
+---#
+
+set enable_gpusort to on;
+set enable_sort to off;
+set log_min_messages = debug1;
+set client_min_messages to warning;
+
+-- #137
+drop table if exists i137;
+create table i137(id integer,t1 text collate "C",t2 text collate "C");
+insert into i137 select generate_series(1,100) , 'i137','i137';
+select * from i137  where id%2=0 order by t1,t2;
---
+++ test/sql/text_gso.sql
@@ -0,0 +1,24 @@
+--#
+--#       Gpu Text Sort TestCases. 
+--#
+
+set gpu_setup_cost=0;
+set random_page_cost=1000000;   --# force off index_scan.   
+set enable_gpusort to on;                                                                                                                                       
+set client_min_messages to warning;
+
+
+select * from (select row_number() over (order by char_x desc) as rowid,id from strom_string_test) as t where t.rowid%100=0;
+select * from (select row_number() over (order by char_x asc) as rowid,id from strom_string_test) as t where t.rowid%100=0;
+
+select * from (select row_number() over (order by nchar_x desc) as rowid,id from strom_string_test) as t where t.rowid%100=0;
+select * from (select row_number() over (order by nchar_x asc) as rowid,id from strom_string_test) as t where t.rowid%100=0;
+
+select * from (select row_number() over (order by vchar_x desc) as rowid,id from strom_string_test) as t where t.rowid%100=0;
+set select * from (select row_number() over (order by vchar_x asc) as rowid,id from strom_string_test) as t where t.rowid%100=0;
+
+select * from (select row_number() over (order by nvchar_x desc) as rowid,id from strom_string_test) as t where t.rowid%100=0;
+select * from (select row_number() over (order by nvchar_x asc) as rowid,id from strom_string_test) as t where t.rowid%100=0;
+
+select * from (select row_number() over (order by text_x desc) as rowid,id from strom_string_test) as t where t.rowid%100=0;
+select * from (select row_number() over (order by text_x asc) as rowid,id from strom_string_test) as t where t.rowid%100=0;
