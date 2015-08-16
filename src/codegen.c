5899474d67d28b0c3643c66eb43999489f0e6e9a
KaiGai Kohei <kaigai@kaigai.gr.jp>
2015-08-16 13:05:04 +0900

    bugfix: codegen always said CaseExpr is unable to run on GPUs

--- src/codegen.c
+++ src/codegen.c
@@ -1956,6 +1956,7 @@ pgstrom_codegen_available_expression(Expr *expr)
 		}
 		if (!pgstrom_codegen_available_expression((Expr *)caseexpr->defresult))
 			return false;
+		return true;
 	}
 	elog(DEBUG2, "Unable to run on device: %s", nodeToString(expr));
 	return false;
