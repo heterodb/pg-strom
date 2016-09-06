775d974781fb653080bb1c00c516a2bed148a85e
KaiGai Kohei <kaigai@kaigai.gr.jp>
2016-09-06 15:45:49 +0900

    refresh nvme_strom.h

--- src/nvme_strom.h
+++ src/nvme_strom.h
@@ -14,10 +14,11 @@ enum {
 	STROM_IOCTL__CHECK_FILE				= _IO('S',0x80),
 	STROM_IOCTL__MAP_GPU_MEMORY			= _IO('S',0x81),
 	STROM_IOCTL__UNMAP_GPU_MEMORY		= _IO('S',0x82),
-	STROM_IOCTL__INFO_GPU_MEMORY		= _IO('S',0x83),
-	STROM_IOCTL__MEMCPY_SSD2GPU			= _IO('S',0x84),
-	STROM_IOCTL__MEMCPY_SSD2GPU_ASYNC	= _IO('S',0x85),
-	STROM_IOCTL__MEMCPY_SSD2GPU_WAIT	= _IO('S',0x86),
+	STROM_IOCTL__LIST_GPU_MEMORY		= _IO('S',0x83),
+	STROM_IOCTL__INFO_GPU_MEMORY		= _IO('S',0x84),
+	STROM_IOCTL__MEMCPY_SSD2GPU			= _IO('S',0x85),
+	STROM_IOCTL__MEMCPY_SSD2GPU_ASYNC	= _IO('S',0x86),
+	STROM_IOCTL__MEMCPY_SSD2GPU_WAIT	= _IO('S',0x87),
 };
 
 /* path of ioctl(2) entrypoint */
@@ -48,18 +49,27 @@ struct StromCmd__UnmapGpuMemory
 };
 typedef struct StromCmd__UnmapGpuMemory	StromCmd__UnmapGpuMemory;
 
+/* STROM_IOCTL__LIST_GPU_MEMORY */
+struct StromCmd__ListGpuMemory
+{
+	uint32_t		nrooms;		/* in: length of the @handles array */
+	uint32_t		nitems;		/* out: number of mapped region */
+	unsigned long	handles[1];	/* out: array of mapped region handles */
+};
+typedef struct StromCmd__ListGpuMemory	StromCmd__ListGpuMemory;
+
 /* STROM_IOCTL__INFO_GPU_MEMORY */
 struct StromCmd__InfoGpuMemory
 {
 	unsigned long	handle;		/* in: handler of the mapped region */
 	uint32_t		nrooms;		/* in: length of the variable length array */
+	uint32_t		nitems;		/* out: number of GPU pages */
 	uint32_t		version;	/* out: 'version' of the page tables */
 	uint32_t		gpu_page_sz;/* out: 'page_size' in bytes */
-	uint32_t		nitems;		/* out: number of GPU pages */
-	struct {
-		void	   *vaddr;		/* out: io-mapped virtual address */
-		uint64_t	paddr;		/* out: physical address */
-	} pages[1];
+	uint32_t		owner;		/* out: UID of the owner */
+	unsigned long	map_offset;	/* out: offset of valid area from the head */
+	unsigned long	map_length;	/* out: length of valid area */
+	uint64_t		paddrs[1];	/* out: array of physical addresses */
 };
 typedef struct StromCmd__InfoGpuMemory	StromCmd__InfoGpuMemory;
 
