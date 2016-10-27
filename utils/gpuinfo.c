11abe3b7407fe93f57e3a05cd7040b206c850cf6
KaiGai Kohei <kaigai@kaigai.gr.jp>
2016-10-27 23:15:08 +0900

    gpuinfo command revised

--- utils/gpuinfo.c
+++ utils/gpuinfo.c
@@ -67,13 +67,13 @@ __error_exit(const char *file_name, int lineno,
 #define ATTRCLASS_INT			1
 #define ATTRCLASS_BYTES			2
 #define ATTRCLASS_KB			3
-#define ATTRCLASS_MB			4
-#define ATTRCLASS_KHZ			5
-#define ATTRCLASS_COMPUTEMODE	6
-#define ATTRCLASS_BOOL			7
+#define ATTRCLASS_KHZ			4
+#define ATTRCLASS_COMPUTEMODE	5
+#define ATTRCLASS_BOOL			6
 
-#define ATTR_ENTRY(label,desc,class,is_detail)		\
-	{ label, ATTRCLASS_##class, desc, #label, is_detail }
+#define DEV_ATTR(label,class,only_detail, desc)		\
+	{ CU_DEVICE_ATTRIBUTE_##label,					\
+	  ATTRCLASS_##class, desc, #label, only_detail }
 
 static struct
 {
@@ -83,180 +83,9 @@ static struct
 	const char *attname_m;
 	int			attdetail;	/* skip without -d option */
 } attribute_catalog[] = {
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK,
-			   "Maximum number of threads per block", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_X,
-			   "Maximum block dimension X", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Y,
-			   "Maximum block dimension Y", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Z,
-			   "Maximum block dimension Z", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_X,
-			   "Maximum grid dimension X", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Y,
-			   "Maximum grid dimension Y", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Z,
-			   "Maximum grid dimension Z", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK,
-			   "Maximum shared memory available per block in bytes", KB, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_TOTAL_CONSTANT_MEMORY,
-			   "Memory available on device for __constant__ variables",
-			   BYTES, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_WARP_SIZE,
-			   "Warp size in threads", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_PITCH,
-			   "Maximum pitch in bytes allowed by memory copies", BYTES, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_REGISTERS_PER_BLOCK,
-			   "Maximum number of 32-bit registers available per block",
-			   INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_CLOCK_RATE,
-			   "Typical clock frequency in kilohertz", KHZ, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_TEXTURE_ALIGNMENT,
-			   "Alignment requirement for textures", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT,
-			   "Number of multiprocessors on device", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_KERNEL_EXEC_TIMEOUT,
-			   "Specifies whether there is a run time limit on kernels",
-			   INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_INTEGRATED,
-			   "Device is integrated with host memory", BOOL, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_CAN_MAP_HOST_MEMORY,
-			   "Device can map host memory into CUDA address space", BOOL, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_COMPUTE_MODE,
-			   "Compute mode (See CUcomputemode for details)", COMPUTEMODE, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_WIDTH,
-			   "Maximum 1D texture width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_WIDTH,
-			   "Maximum 2D texture width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_HEIGHT,
-			   "Maximum 2D texture height", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_WIDTH,
-			   "Maximum 3D texture width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_HEIGHT,
-			   "Maximum 3D texture height", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_DEPTH,
-			   "Maximum 3D texture depth", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LAYERED_WIDTH,
-			   "Maximum 2D layered texture width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LAYERED_HEIGHT,
-			   "Maximum 2D layered texture height", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LAYERED_LAYERS,
-			   "Maximum layers in a 2D layered texture", INT,1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_SURFACE_ALIGNMENT,
-			   "Alignment requirement for surfaces", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_CONCURRENT_KERNELS,
-			   "Device can possibly execute multiple kernels concurrently",
-			   BOOL, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_ECC_ENABLED,
-			   "Device has ECC support enabled", BOOL, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_PCI_BUS_ID,
-			   "PCI bus ID of the device", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_PCI_DEVICE_ID,
-			   "PCI device ID of the device", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_TCC_DRIVER,
-			   "Device is using TCC driver model", BOOL, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE,
-			   "Peak memory clock frequency in kilohertz", KHZ, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_GLOBAL_MEMORY_BUS_WIDTH,
-			   "Global memory bus width in bits", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_L2_CACHE_SIZE,
-			   "Size of L2 cache in bytes", BYTES, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_MULTIPROCESSOR,
-			   "Maximum resident threads per multiprocessor", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_ASYNC_ENGINE_COUNT,
-			   "Number of asynchronous engines", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_UNIFIED_ADDRESSING,
-			   "Device shares a unified address space with the host", BOOL, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LAYERED_WIDTH,
-			   "Maximum 1D layered texture width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LAYERED_LAYERS,
-			   "Maximum layers in a 1D layered texture", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_GATHER_WIDTH,
-			"Maximum 2D texture width if CUDA_ARRAY3D_TEXTURE_GATHER is set",
-			   INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_GATHER_HEIGHT,
-			"Maximum 2D texture height if CUDA_ARRAY3D_TEXTURE_GATHER is set",
-			   INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_WIDTH_ALTERNATE,
-			   "Alternate maximum 3D texture width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_HEIGHT_ALTERNATE,
-			   "Alternate maximum 3D texture height", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_DEPTH_ALTERNATE,
-			   "Alternate maximum 3D texture depth", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_PCI_DOMAIN_ID,
-			   "PCI domain ID of the device", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_TEXTURE_PITCH_ALIGNMENT,
-			   "Pitch alignment requirement for textures", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURECUBEMAP_WIDTH,
-			   "Maximum cubemap texture width/height", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURECUBEMAP_LAYERED_WIDTH,
-			   "Maximum cubemap layered texture width/height", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURECUBEMAP_LAYERED_LAYERS,
-			   "Maximum layers in a cubemap layered texture", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE1D_WIDTH,
-			   "Maximum 1D surface width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE2D_WIDTH,
-			   "Maximum 2D surface width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE2D_HEIGHT,
-			   "Maximum 2D surface height", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE3D_WIDTH,
-			   "Maximum 3D surface width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE3D_HEIGHT,
-			   "Maximum 3D surface height", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE3D_DEPTH,
-			   "Maximum 3D surface depth", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE1D_LAYERED_WIDTH,
-			   "Maximum 1D layered surface width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE1D_LAYERED_LAYERS,
-			   "Maximum layers in a 1D layered surface", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE2D_LAYERED_WIDTH,
-			   "Maximum 2D layered surface width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE2D_LAYERED_HEIGHT,
-			   "Maximum 2D layered surface height", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE2D_LAYERED_LAYERS,
-			   "Maximum layers in a 2D layered surface", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACECUBEMAP_WIDTH,
-			   "Maximum cubemap surface width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACECUBEMAP_LAYERED_WIDTH,
-			   "Maximum cubemap layered surface width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACECUBEMAP_LAYERED_LAYERS,
-			   "Maximum layers in a cubemap layered surface", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LINEAR_WIDTH,
-			   "Maximum 1D linear texture width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LINEAR_WIDTH,
-			   "Maximum 2D linear texture width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LINEAR_HEIGHT,
-			   "Maximum 2D linear texture height", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LINEAR_PITCH,
-			   "Maximum 2D linear texture pitch in bytes", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_MIPMAPPED_WIDTH,
-			   "Maximum mipmapped 2D texture width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_MIPMAPPED_HEIGHT,
-			   "Maximum mipmapped 2D texture height", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR,
-			   "Major compute capability version number", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR,
-			   "Minor compute capability version number", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_MIPMAPPED_WIDTH,
-			   "Maximum mipmapped 1D texture width", INT, 1),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_STREAM_PRIORITIES_SUPPORTED,
-			   "Device supports stream priorities", BOOL, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_GLOBAL_L1_CACHE_SUPPORTED,
-			   "Device supports caching globals in L1", BOOL, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_LOCAL_L1_CACHE_SUPPORTED,
-			   "Device supports caching locals in L1", BOOL, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_MULTIPROCESSOR,
-			   "Maximum shared memory available per multiprocessor", BYTES, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MAX_REGISTERS_PER_MULTIPROCESSOR,
-			   "Maximum number of 32bit registers per multiprocessor", INT, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MANAGED_MEMORY,
-			   "Device can allocate managed memory on this system", BOOL, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MULTI_GPU_BOARD,
-			   "Device is on a multi-GPU board", BOOL, 0),
-	ATTR_ENTRY(CU_DEVICE_ATTRIBUTE_MULTI_GPU_BOARD_GROUP_ID,
-			   "Unique id for a group of devices on the same multi-GPU board",
-			   INT, 0),
+#include "../src/device_attrs.h"
 };
+#undef DEV_ATTR
 
 static void output_device(CUdevice device, int dev_id)
 {
@@ -270,7 +99,7 @@ static void output_device(CUdevice device, int dev_id)
 	if (!machine_format)
 		printf("--------\nDevice Identifier: %d\n", dev_id);
 	else
-		printf("CU_DEVICE_ATTRIBUTE_DEVICE_ID=%d\n", dev_id);
+		printf("DEVICE%d:DEVICE_ID=%d\n", dev_id, dev_id);
 
 	/* device name */
 	rc = cuDeviceGetName(dev_name, sizeof(dev_name), device);
@@ -279,7 +108,7 @@ static void output_device(CUdevice device, int dev_id)
 	if (!machine_format)
 		printf("Device Name: %s\n", dev_name);
 	else
-		printf("CU_DEVICE_ATTRIBUTE_DEVICE_NAME=%s\n", dev_name);
+		printf("DEVICE%d:DEVICE_NAME=%s\n", dev_id, dev_name);
 
 	/* device RAM size */
 	rc = cuDeviceTotalMem(&dev_memsz, device);
@@ -288,7 +117,7 @@ static void output_device(CUdevice device, int dev_id)
 	if (!machine_format)
 		printf("Global memory size: %zuMB\n", dev_memsz >> 20);
 	else
-		printf("CU_DEVICE_ATTRIBUTE_GLOBAL_MEMORY_SIZE=%zu\n", dev_memsz);
+		printf("DEVICE%d:GLOBAL_MEMORY_SIZE=%zu\n", dev_id, dev_memsz);
 
 	for (i=0; i < lengthof(attribute_catalog); i++)
 	{
@@ -313,35 +142,28 @@ static void output_device(CUdevice device, int dev_id)
 				if (!machine_format)
 					printf("%s: %d\n", attname_h, dev_prop);
 				else
-					printf("%s=%d\n", attname_m, dev_prop);
+					printf("DEVICE%d:%s=%d\n", dev_id, attname_m, dev_prop);
 				break;
 
 			case ATTRCLASS_BYTES:
 				if (!machine_format)
 					printf("%s: %dbytes\n", attname_h, dev_prop);
 				else
-					printf("%s=%d\n", attname_m, dev_prop);
+					printf("DEVICE%d:%s=%d\n", dev_id, attname_m, dev_prop);
 				break;
 
 			case ATTRCLASS_KB:
 				if (!machine_format)
 					printf("%s: %dKB\n", attname_h, dev_prop);
 				else
-					printf("%s=%d\n", attname_m, dev_prop);
-				break;
-
-			case ATTRCLASS_MB:
-				if (!machine_format)
-					printf("%s: %dMB\n", attname_h, dev_prop);
-				else
-					printf("%s=%d\n", attname_m, dev_prop);
+					printf("DEVICE%d:%s=%d\n", dev_id, attname_m, dev_prop);
 				break;
 
 			case ATTRCLASS_KHZ:
 				if (!machine_format)
 					printf("%s: %dKHZ\n", attname_h, dev_prop);
 				else
-					printf("%s=%d\n", attname_m, dev_prop);
+					printf("DEVICE%d:%s=%d\n", dev_id, attname_m, dev_prop);
 				break;
 
 			case ATTRCLASS_COMPUTEMODE:
@@ -368,7 +190,7 @@ static void output_device(CUdevice device, int dev_id)
 				if (!machine_format)
 					printf("%s: %s\n", attname_h, temp);
 				else
-					printf("%s=%s\n", attname_m, temp);
+					printf("DEVICE%d:%s=%s\n", dev_id, attname_m, temp);
 				break;
 
 			case ATTRCLASS_BOOL:
@@ -376,7 +198,7 @@ static void output_device(CUdevice device, int dev_id)
 					printf("%s: %s\n", attname_h,
 						   dev_prop ? "true" : "false");
 				else
-					printf("%s=%s\n", attname_m,
+					printf("DEVICE%d:%s=%s\n", dev_id, attname_m,
 						   dev_prop ? "true" : "false");
 				break;
 
@@ -384,7 +206,7 @@ static void output_device(CUdevice device, int dev_id)
 				if (!machine_format)
 					printf("%s: %d\n", attname_h, dev_prop);
 				else
-					printf("%s=%d\n", attname_m, dev_prop);
+					printf("DEVICE%d:%s=%d\n", dev_id, attname_m, dev_prop);
 				break;
 		}
 	}
@@ -441,7 +263,7 @@ int main(int argc, char *argv[])
 			   (version % 1000) / 10,
 			   (version % 10));
 	else
-		printf("CU_PLATFORM_ATTRIBUTE_CUDA_RUNTIME_VERSION=%d.%d.%d\n",
+		printf("PLATFORM:CUDA_RUNTIME_VERSION=%d.%d.%d\n",
 			   (version / 1000),
                (version % 1000) / 10,
                (version % 10));
@@ -459,7 +281,7 @@ int main(int argc, char *argv[])
 			if (!machine_format)
 				printf("NVIDIA Driver version: %d.%d\n", major, minor);
 			else
-				printf("CU_PLATFORM_ATTRIBUTE_NVIDIA_DRIVER_VERSION=%d.%d\n",
+				printf("PLATFORM:NVIDIA_DRIVER_VERSION=%d.%d\n",
 					   major, minor);
 		}
 		fclose(filp);
@@ -475,7 +297,7 @@ int main(int argc, char *argv[])
 	if (!machine_format)
 		printf("Number of devices: %d\n", count);
 	else
-		printf("CU_PLATFORM_ATTRIBUTE_NUMBER_OF_DEVICES=%d\n", count);
+		printf("PLATFORM:NUMBER_OF_DEVICES=%d\n", count);
 
 	for (i=0; i < count; i++)
 	{
