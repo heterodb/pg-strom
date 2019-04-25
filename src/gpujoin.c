0685e9d89ffd4fdb0f7b09b3f3e0c15cf86a062b
KaiGai Kohei <kaigai@heterodb.com>
2019-04-25 10:10:02 +0900

    remove cost discount by num_partition_siblings; noise is bigger than benefit.

--- src/gpujoin.c
+++ src/gpujoin.c
@@ -343,8 +343,6 @@ static bool					enable_gpunestloop;				/* GUC */
 static bool					enable_gpuhashjoin;				/* GUC */
 static bool					enable_partitionwise_gpujoin;	/* GUC */
 
-static int					num_partition_siblings = 0;
-
 /* static functions */
 static void gpujoin_switch_task(GpuTaskState *gts, GpuTask *gtask);
 static GpuTask *gpujoin_next_task(GpuTaskState *gts);
@@ -562,7 +560,6 @@ cost_gpujoin(PlannerInfo *root,
 	double		parallel_divisor = 1.0;
 	double		num_chunks;
 	double		outer_ntuples;
-	Cost		inner_cost;
 	int			i, num_rels = gpath->num_rels;
 	bool		retval = false;
 
@@ -590,10 +587,7 @@ cost_gpujoin(PlannerInfo *root,
 	}
 	else
 	{
-		startup_cost = pgstrom_gpu_setup_cost;
-		if (num_partition_siblings > 0)
-			startup_cost /= (Cost)num_partition_siblings;
-		startup_cost += outer_path->startup_cost;
+		startup_cost = pgstrom_gpu_setup_cost + outer_path->startup_cost;
 		run_cost = outer_path->total_cost - outer_path->startup_cost;
 		num_chunks = estimate_num_chunks(outer_path);
 	}
@@ -646,10 +640,7 @@ cost_gpujoin(PlannerInfo *root,
 		}
 
 		/* cost to load all the tuples from inner-path */
-		inner_cost = scan_path->total_cost;
-		if (num_partition_siblings > 0)
-			inner_cost /= (Cost)num_partition_siblings;
-		startup_cost += inner_cost;
+		startup_cost += scan_path->total_cost;
 
 		/* cost for join_qual startup */
 		cost_qual_eval(&join_quals_cost, join_quals, root);
@@ -672,10 +663,7 @@ cost_gpujoin(PlannerInfo *root,
 				(double)__KDS_NSLOTS((Size)scan_path->rows);
 
 			/* cost to compute inner hash value by CPU */
-			inner_cost = (cpu_operator_cost * num_hashkeys * scan_path->rows);
-			if (num_partition_siblings > 0)
-				inner_cost /= (Cost) num_partition_siblings;
-			startup_cost += inner_cost;
+			startup_cost += cpu_operator_cost * num_hashkeys * scan_path->rows;
 
 			/* cost to comput hash value by GPU */
 			run_cost += (pgstrom_gpu_operator_cost *
@@ -696,10 +684,7 @@ cost_gpujoin(PlannerInfo *root,
 			double		inner_ntuples = scan_path->rows;
 
 			/* cost to preload inner heap tuples by CPU */
-			inner_cost = cpu_tuple_cost * inner_ntuples;
-			if (num_partition_siblings > 0)
-				inner_cost /= (Cost) num_partition_siblings;
-			startup_cost += inner_cost;
+			startup_cost += cpu_tuple_cost * inner_ntuples;
 
 			/* cost to evaluate join qualifiers */
 			run_cost_per_chunk += (join_quals_cost.per_tuple *
@@ -712,11 +697,8 @@ cost_gpujoin(PlannerInfo *root,
 	/* outer DMA send cost */
 	run_cost += (double)num_chunks * pgstrom_gpu_dma_cost;
 	/* inner DMA send cost */
-	inner_cost = ((double)inner_buffer_sz /
-				  (double)pgstrom_chunk_size()) * pgstrom_gpu_dma_cost;
-	if (num_partition_siblings > 0)
-		inner_cost /= (Cost) num_partition_siblings;
-	run_cost += inner_cost;
+	run_cost += ((double)inner_buffer_sz /
+				 (double)pgstrom_chunk_size()) * pgstrom_gpu_dma_cost;
 	/* cost for projection */
 	startup_cost += joinrel->reltarget->cost.startup;
 	run_cost += joinrel->reltarget->cost.per_tuple * gpath->cpath.path.rows;
@@ -1439,12 +1421,6 @@ extract_partitionwise_pathlist(PlannerInfo *root,
 		int			parallel_nworkers = 0;
 		bool		all_gpujoin = true;
 
-		/*
-		 * tells cost estimator this path is a part of partition-wise
-		 * query execution.
-		 */
-		num_partition_siblings = list_length(append_path->subpaths);
-
 		/* temporary disables "dynamic programming" algorithm */
 		root->join_rel_level = NULL;
 		foreach (lc1, append_path->subpaths)
@@ -1609,7 +1585,6 @@ extract_partitionwise_pathlist(PlannerInfo *root,
 		*p_partitioned_rels  = append_path->partitioned_rels;
 	skip:
 		/* restore */
-		num_partition_siblings = 0;
 		root->join_rel_level = join_rel_level_saved;
 		root->join_info_list = join_info_list_saved;
 		enable_gpunestloop = enable_gpunestloop_saved;
@@ -1618,7 +1593,6 @@ extract_partitionwise_pathlist(PlannerInfo *root,
 	PG_CATCH();
 	{
 		/* restore */
-		num_partition_siblings = 0;
 		root->join_rel_level = join_rel_level_saved;
 		root->join_info_list = join_info_list_saved;
 		enable_gpunestloop = enable_gpunestloop_saved;
