58f8a48f83f1376f71edc48a7102d9b7e5cece7f
KaiGai Kohei <kaigai@heterodb.com>
2021-07-28 23:47:26 +0900

    gpu_cache: add 'gpu_buffer_corrupted' state
    if kern_gpucache_apply_redo() was failed (likely, more rows than
    max_num_rows were required), GPU cache marks corruption state not
    to use inconsistent GPU cache.
    
    it must be recovered by pgstrom.gpucache_recovery() manually.

--- sql/pg_strom--3.0--3.1.sql
+++ sql/pg_strom--3.0--3.1.sql
@@ -1,4 +1,12 @@
 ---
+--- GPU Cache Functions
+---
+CREATE FUNCTION pgstrom.gpucache_recovery(regclass)
+  RETURNS bigint
+  AS 'MODULE_PATHNAME','pgstrom_gpucache_recovery'
+  LANGUAGE C CALLED ON NULL INPUT;
+
+---
 --- Deprecated Functions
 ---
 DROP FUNCTION IF EXISTS pgstrom.arrow_fdw_export_cupy(regclass, text[], int);
