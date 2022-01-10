0387928bc24725ad502cbd1d9b818b8a2232fd37
onishi <onishi@heterodb.com>
2022-01-10 16:57:48 +0900

    progress

--- fluentd/fluent-plugin-arrow-file/test/plugin/test_out_arrow_file.rb
+++ fluentd/fluent-plugin-arrow-file/test/plugin/test_out_arrow_file.rb
@@ -1,29 +1,43 @@
 require "helper"
 require "fluent/plugin/out_arrow_file.rb"
 
+# テスト実行方法
+# cd ~/pg-strom/fluentd/fluent-plugin-arrow-file
+# bundle exec rake test
+
 class ArrowFileOutputTest < Test::Unit::TestCase
   TMP_DIR = File.expand_path(File.dirname(__FILE__) + "/../tmp/out_file#{ENV['TEST_ENV_NUMBER']}")
 
-  DEF_CONFIG = %[
+  DEFAULT_CONFIG = %[
     path #{TMP_DIR}/arrow_file_test.arrow
     schema_defs "uint8_column=Uint8"
   ]
 
   def setup
     Fluent::Test.setup
+    FileUtils.mkdir_p TMP_DIR
   end
 
-  test "mytest" do
-    d = create_driver %[
-      path /tmp/test_path
-      schema_defs "uint8_column=Uint8"
-    ]
-    assert_equal '/tmp/test_path',d.instance.path 
+  test "conf_paths" do
+    d = create_driver
+    assert_equal "#{TMP_DIR}/arrow_file_test.arrow",d.instance.path 
   end
 
-  private
+  test "feed_test" do
+    d = create_driver
+    d.run(default_tag: 'test_tag') do
+      d.feed({'uint8_column' => 93})
+    end
 
-  def create_driver(conf)
-    Fluent::Test::Driver::Output.new(Fluent::Plugin::ArrowFileOutput).configure(conf)
+    assert system("../../arrow-tools/arrow2csv --header #{TMP_DIR}/arrow_file_test.arrow")
+  end
+
+  teardown do
+    FileUtils.rm_rf TMP_DIR
+  end
+
+
+  def create_driver(conf = DEFAULT_CONFIG,opts={})
+    Fluent::Test::Driver::Output.new(Fluent::Plugin::ArrowFileOutput, opts: opts).configure(conf)
   end
 end
