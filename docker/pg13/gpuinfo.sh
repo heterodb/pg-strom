bb7e3561f8c5686a938c6fe0522026d1c655d00f
Eric Murphy <murphye@gmail.com>
2022-11-08 16:40:03 -0800

    Delete gpuinfo.sh

--- docker/pg13/gpuinfo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# When running in a VM or container, the NVIDIA driver will likely not be available under '/sys/module/nvidia/version'
-# This script is a hack that prepends a NVIDIA_DRIVER_VERSION value before the `gpuinfo` output so PG-Strom will start up.
-# For more information, please see https://github.com/heterodb/pg-strom/issues/552#issuecomment-1288449738
-echo "PLATFORM:NVIDIA_DRIVER_VERSION=000.00"
-gpuinfo.orig -md
