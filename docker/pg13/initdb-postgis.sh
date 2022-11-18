be8ad8e50e093ce5899e10dfe3fc4c2cca8ba70c
Eric Murphy <murphye@gmail.com>
2022-11-17 17:55:27 -0800

    Reenable address_standardizer_data_us

--- docker/pg13/initdb-postgis.sh
+++ docker/pg13/initdb-postgis.sh
@@ -21,7 +21,7 @@ for DB in template_postgis "$POSTGRES_DB"; do
 		\c
 		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
 		CREATE EXTENSION IF NOT EXISTS address_standardizer;
---		CREATE EXTENSION IF NOT EXISTS address_standardizer_data_us; 
+		CREATE EXTENSION IF NOT EXISTS address_standardizer_data_us; 
 		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
         CREATE EXTENSION IF NOT EXISTS pg_strom;
 EOSQL
