fadc16c222e5b99680bcebab1d22f94e31dd3a2d
KaiGai Kohei <kaigai@heterodb.com>
2023-05-18 00:15:57 +0900

    adjust epoch difference from arrow to postgresql

--- src/xpu_common.cu
+++ src/xpu_common.cu
@@ -1141,7 +1141,8 @@ __arrow_fetch_timestamp_datum(kern_context *kcxt,
 			{
 				uint64_t   *base = (uint64_t *)
 					((char *)kds + __kds_unpack(cmeta->values_offset));
-				kvar->u64 = base[kds_index] * 1000000L;
+				kvar->u64 = base[kds_index] * 1000000L -
+					(POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * USECS_PER_DAY;
 				*vclass = KVAR_CLASS__INLINE;
 				return true;
 			}
@@ -1153,7 +1154,8 @@ __arrow_fetch_timestamp_datum(kern_context *kcxt,
 			{
 				uint64_t   *base = (uint64_t *)
 					((char *)kds + __kds_unpack(cmeta->values_offset));
-				kvar->u64 = base[kds_index] * 1000L;
+				kvar->u64 = base[kds_index] * 1000L -
+					(POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * USECS_PER_DAY;
 				*vclass = KVAR_CLASS__INLINE;
 				return true;
 			}
@@ -1165,7 +1167,8 @@ __arrow_fetch_timestamp_datum(kern_context *kcxt,
 			{
 				uint64_t   *base = (uint64_t *)
 					((char *)kds + __kds_unpack(cmeta->values_offset));
-				kvar->u64 = base[kds_index];
+				kvar->u64 = base[kds_index] -
+					(POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * USECS_PER_DAY;
 				*vclass = KVAR_CLASS__INLINE;
 				return true;
 			}
@@ -1177,7 +1180,8 @@ __arrow_fetch_timestamp_datum(kern_context *kcxt,
 			{
 				uint64_t   *base = (uint64_t *)
 					((char *)kds + __kds_unpack(cmeta->values_offset));
-				kvar->u64 = base[kds_index] / 1000L;
+				kvar->u64 = base[kds_index] / 1000L -
+					(POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * USECS_PER_DAY;
 				*vclass = KVAR_CLASS__INLINE;
 				return true;
 			}
