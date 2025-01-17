5011be1e3c386124acf5aa1f4155afd062e4b3d5
0-kaz <78154587+0-kaz@users.noreply.github.com>
2025-01-17 10:27:04 +0900

    Feat/heavy data test (#859)
    * modify .source files
    
    * modify .source file in test/13/ouput
    
    * fix gpu_cache for PG12
    
    * fix regression test for gpu_cache PG13
    
    * progress
    
    * commit working correctly
    
    * add load.sh
    
    * fix
    
    * add one tpch query
    
    * add query sql files

---
+++ test/heavy_data_test/query/use_pinned_buffer_1_select_query.sql
@@ -0,0 +1,35 @@
+SET search_path=tpch;
+DROP TABLE IF EXISTS result_store;
+SET pg_strom.gpudirect_enabled = OFF; SET pg_strom.pinned_inner_buffer_threshold = 10;
+EXPLAIN ANALYZE
+select supp_nation,
+       cust_nation,
+       l_year,
+       sum(volume) as revenue
+  into result_store 
+  from (select n1.n_name as supp_nation,
+               n2.n_name as cust_nation,
+               extract(year from l_shipdate) as l_year,
+               l_extendedprice * (1 - l_discount) as volume
+          from supplier,
+               lineitem,
+               orders,
+               customer,
+               nation n1,
+               nation n2
+         where s_suppkey = l_suppkey
+           and o_orderkey = l_orderkey
+           and c_custkey = o_custkey
+           and s_nationkey = n1.n_nationkey
+           and c_nationkey = n2.n_nationkey
+           and ((n1.n_name = 'UNITED STATES' and n2.n_name = 'INDIA') or
+		(n1.n_name = 'INDIA' and n2.n_name = 'UNITED STATES'))
+           and o_orderdate between date '1995-01-01' and date '1996-12-31'
+       ) as shipping
+ group by supp_nation,
+          cust_nation,
+          l_year
+ order by supp_nation,
+          cust_nation,
+          l_year;
+select * from result_store;
