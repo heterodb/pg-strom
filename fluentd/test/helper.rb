afa42285cb1961c1de79f87da855737d843c0bac
0-kaz <78154587+0-kaz@users.noreply.github.com>
2026-01-23 09:17:56 +0900

    Feat/fluentd test update (#992)
    * modify .source files
    
    * modify .source file in test/13/ouput
    
    * fix gpu_cache for PG12
    
    * fix regression test for gpu_cache PG13
    
    * progress
    
    * commit working correctly
    
    * for 17
    
    * add python data compare
    
    * fix to pass build error
    
    * progress
    
    * update for column_replace
    
    * support v6

--- fluentd/test/helper.rb
+++ fluentd/test/helper.rb
@@ -1,4 +1,19 @@
 $LOAD_PATH.unshift(File.expand_path("../../", __FILE__))
+
+# NOTE: Suppress warnings about frozen string literals from Fluentd gem
+#
+# This warning originates from Fluentd 1.19.1 gem's internal code:
+#   /opt/fluent/lib/ruby/gems/3.4.0/gems/fluentd-1.19.1/lib/fluent/plugin/buffer/memory_chunk.rb:25
+#   @chunk = ''.force_encoding(Encoding::ASCII_8BIT)
+#
+# This is a bug in Fluentd gem itself, not in our codebase.
+# The warning will be resolved when Fluentd updates to support Ruby 3.4's frozen string literals.
+# Until then, we suppress these warnings to keep test output clean.
+#
+# Reference: In Ruby 3.4+, string literals will be frozen by default.
+# The Fluentd gem should use String.new('') or +''.force_encoding(...) instead.
+Warning[:deprecated] = false if Warning.respond_to?(:[]=)
+
 require "test-unit"
 require "fluent/test"
 require "fluent/test/driver/output"
